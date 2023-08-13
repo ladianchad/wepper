5965b9790a9f121001c1486b4128bea03e4ae8e8
ladianchad <qhrejddlvltm@gmail.com>
2023-08-13 13:30:02 +0900

    initial file structure
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

---
+++ src/main/kotlin/com/wepping/wepper/interface/user/dto/UserDto.kt
@@ -0,0 +1,14 @@
+package com.wepping.wepper.`interface`.user.dto
+
+class UserDto(
+    val id: Long,
+
+    val password: String,
+
+    val userName: String,
+
+    val nickName: String,
+
+    val email: String?
+) {
+}
